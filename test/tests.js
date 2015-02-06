86bed7b26921c3d4ecf4710dc4143973e11ca242
Steven Martin <stevenisekimartin@gmail.com>
2015-02-06 23:40:58 +1100

    update to tests and package.json

--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,7 @@
 var test = require('tape');
 var SecondsTohhmmss = require('../js/SecondsTohhmmss.js');
 
-test('basic arithmetic', function (t) {
+test('Seconds is converted correctly to string output', function (t) {
     t.equal(SecondsTohhmmss(2), "00:00:02");
     t.equal(SecondsTohhmmss(70), "00:01:10");
     t.end();
